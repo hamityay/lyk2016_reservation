4769ad44b755d44e582e0ced05808a38800a7063
Hamit YAY <hamityay@hotmail.com>
2016-08-10 10:21:20 +0300

    Crerated places controller and Placemodel and added index and show action to show values from database

---
+++ app/models/place.rb
@@ -0,0 +1,13 @@
+class Place < ApplicationRecord
+	validates :name, presence: true
+	validates :adress, length: { maximum: 50 }
+	validates :phone_number, length: { minimum: 10 }, uniqueness: true
+	validates :contact_mail, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "not an e-mail adress" }, uniqueness: true
+	validate :check_established_at
+
+	def check_established_at
+		if established_at.present? && established_at < Date.today
+			errors.add(:established_at, "Cannot be past")
+		end
+	end
+end
