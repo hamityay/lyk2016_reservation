5d64cabd082b1cef028bc7103c3fa99b33a04b4a
Hamit YAY <hamityay@hotmail.com>
2016-08-17 12:37:10 +0300

    Generated Food model and join table (migration)

---
+++ db/migrate/20160817093410_create_join_table_foods_places.rb
@@ -0,0 +1,5 @@
+class CreateJoinTableFoodsPlaces < ActiveRecord::Migration[5.0]
+  def change
+    create_join_table :foods, :places
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817083124) do
+ActiveRecord::Schema.define(version: 20160817093410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,17 @@ ActiveRecord::Schema.define(version: 20160817083124) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_costumers_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_costumers_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "foods", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "foods_places", id: false, force: :cascade do |t|
+    t.integer "food_id",  null: false
+    t.integer "place_id", null: false
   end
 
   create_table "owners", force: :cascade do |t|
